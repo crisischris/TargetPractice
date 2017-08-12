96512b9c011d263ee5cfa3635aab6118199e4ab4
crisischris <christopherthomasnelson@gmail.com>
2017-08-11 23:33:55 -0700

    Added start scene
    added set peices to start scene

---
+++ Assets/Scripts/SetMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMovement : MonoBehaviour {
+    private float initX;
+    private float initY;
+    private float initZ;
+    private float deviationX;
+    private float deviationY;
+
+    // Use this for initialization
+    void Start () {
+
+        initX = transform.position.x;
+        initY = transform.position.y;
+        initZ = transform.position.z;
+
+        print("X = " + initX);
+        print("Y = " + initY);
+        print("Z = " + initZ);
+
+        deviationX = 1;
+        deviationY = Random.Range(.35f, .4f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+        transform.position = new Vector3(initX + Mathf.PingPong(Time.time, deviationX), initY + Mathf.PingPong(Time.time, deviationY), initZ);
+
+
+    }
+
+   
+}
---
+++ Assets/Scripts/UI/DestroyOnClickUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DestroyOnClickUI : MonoBehaviour {
+
+    public GameObject sign;
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void OnClick()
+    {
+        Destroy(sign);
+        Destroy(text);
+
+    }
+}
